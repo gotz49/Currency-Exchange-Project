ad745773d80d0400493c9e0e2fc620d97f2a0cbe
Santiago Diaz <gotzdiaz@gmail.com>
2022-07-24 16:35:05 -0300

    nueva rama API, modifico todo para hacer la app mediante una API

--- javascript/main.js
+++ javascript/main.js
@@ -1,115 +1,42 @@
-// Guardo precios de monedas en el localStorage
-const editarPrecios = document.querySelector("#editarPrecios");
-
-editarPrecios.onclick = () => {
-	let precios = document.querySelector("#precios");
-
-	hideAndSeek(precios);
-};
-
-const cambiarPrecio = document.querySelector("#divisas");
-
-cambiarPrecio.onsubmit = () => {
-	const cero = document.querySelector("#usdToArg").value;
-	const uno = document.querySelector("#argToUsd").value;
-	const dos = document.querySelector("#usdToUyu").value;
-	const tres = document.querySelector("#uyuToUsd").value;
-	const cuatro = document.querySelector("#uyuToArg").value;
-	const cinco = document.querySelector("#argToUyu").value;
-
-	const monedas = [
-		{
-			id: 0,
-			valor: cero,
-		},
-		{
-			id: 1,
-			valor: uno,
-		},
-		{
-			id: 2,
-			valor: dos,
-		},
-		{
-			id: 3,
-			valor: tres,
-		},
-		{
-			id: 4,
-			valor: cuatro,
-		},
-		{                             
-			id: 5,
-			valor: cinco,
-		},
-	];
-
-	guardarLocal("Monedas", JSON.stringify(monedas));
-};
-
-// Renderizado de los precios de monedas desde el localStorage
-const monedas = JSON.parse(localStorage.getItem("Monedas"));
-
-let verMonedas = document.querySelector("#verMonedas");
-
-verMonedas.onclick = () => {
-	let monedasDOM = document.querySelector("#monedasDOM");
-
-	hideAndSeek(monedasDOM);
-     
-	monedas === null
-		? swal({
-			title: "Error",
-			text: "No cargaste los tipos de cambio previamente",
-			icon: "error",
-			confirm: "ok", 
-			timer: 2000    
-			})
-		: (monedasDOM.innerHTML = `
-               <h1>Tipos de Cambio</h1>
-               
-               <p>USD -> $ARG = ${busqueda(0).toFixed(2)}</p>
-               <p>$ARG -> USD = ${busqueda(1).toFixed(2)}</p>
-               <p>USD -> $UYU = ${busqueda(2).toFixed(2)}</p>
-               <p>$UYU -> USD = ${busqueda(3).toFixed(2)}</p>
-               <p>$UYU -> $ARG = ${busqueda(4).toFixed(2)}</p>
-               <p>$ARG -> $UYU = ${busqueda(5).toFixed(2)}</p>
-               `);
-};
-
-// eleccion del tipo de cambio
-const eleccionConversor = document.querySelector("#eleccionConversor");
-
-eleccionConversor.onchange = () => {
-	let select = document.querySelector("#eleccionConversor");
-	let value = select.options[select.selectedIndex].value;
-	console.log(value);
-
-	switch (value) {
-		case "sinOpcion":
-			alert("Elija una opción para la conversión que desea hacer.");
-			break;
-
-		case "cero":
-			calculoDivisaPor(0);
-			break;
-		case "uno":
-			calculoDivisaDiv(1);
-			break;
-		case "dos":
-			calculoDivisaPor(2);
-			break;
-		case "tres":
-			calculoDivisaDiv(3);
-			break;
-		case "cuatro":
-			calculoDivisaPor(4);
-			break;
-		case "cinco":
-			calculoDivisaPor(5);
-			break;
-
-		default:
-			break;
+const OPTIONS = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '4761079223msh08938456fea15f9p14e238jsne63d1fee6b21',
+		'X-RapidAPI-Host': 'currency-converter5.p.rapidapi.com'
 	}
 };
+
+
+const fetchConverter= (amount) => {
+	return fetch(`https://currency-converter5.p.rapidapi.com/currency/convert?format=json&from=AUD&to=CAD&amount=${amount}`, OPTIONS)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
+};
+
+const $ = selector => document.querySelector(selector)
+
+const $formMonto = $('#formMonto');
+const $monto = $('#monto');
+const $submit = $('#convertirButton');
+const $resultado = $('#resultado');
+
+
+$formMonto.addEventListener('submit', async (e) => {
+     e.preventDefault();
+     const {value} = $monto
+     if (!value) return
+     
+     $submit.setAttribute('disabled', '')
+     $submit.setAttribute('aria-busy', 'true')
+     
+     const conversion = await fetchConverter(Number(value));
+     
+     $resultado.innerHTML = JSON.stringify(conversion, null, 2)
+     
+     
+     console.log(Number(value));
+     
+     $submit.removeAttribute('disabled')
+     $submit.removeAttribute('aria-busy')
+})
